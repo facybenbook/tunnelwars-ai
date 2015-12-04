caaac10bac4ca87af8a00bf343d0ddf051c9e3c1
Gabe Montague <gmontague@college.harvard.edu>
2015-12-04 14:01:54 -0500

    Strategy is now a class. Util.Key is now only in QLearning

--- Assets/AI/AIAgent.cs
+++ Assets/AI/AIAgent.cs
@@ -9,20 +9,20 @@
  * 		Transition model: Advancement of real gameplay
  * 		Action model: Valid key actions
  * 		Terminal state: Winning (1) or losing (-1)
- * 		Heuristic: Strategy-weighted World evaluation + Level 2 conformance
+ * 		Heuristic: StrategyType-weighted World evaluation + Level 2 conformance
  * 
  * Level 2 - Bloxel-level classical A* search
  * 		State space: BlockWorld's
  * 		Action space: Simplified directions
  * 		Transition model: Move in direction, taking out block if necessary
- * 		Goal state: Strategy specific
+ * 		Goal state: StrategyType specific
  * 		Action model: Must have ammo to blow out blocks. Must have walls if climbing
- * 		Cost function: Step number, Strategy-weighted BlockWorld evaluation (dominate)
+ * 		Cost function: Step number, StrategyType-weighted BlockWorld evaluation (dominate)
  * 		Heuristic: Distance (Euclidean if above ground)
  * 
  * Level 3 - Q-learned strategy selection
  * 		State space: SimplifiedWorld's
- * 		Action space: Strategy
+ * 		Action space: StrategyType
  * 		Transition model: Set strategy
  * 		Action model: All, except only dig down once.
  * 		Reward function: Delta health, winning the game (dominating term)
---
+++ Assets/AI/Strategy.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * StrategyType.cs
+ * 
+ * The strategy class encapsulates all strategy specific logic. A strategy influences
+ * both Level 1 in the form of weights, but also Level 2 in the form of the AStar
+ * 
+ * 
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public enum StrategyType {
+	Attack,
+	RunAway,
+	GetAmmo,
+	DigDown
+}
+
+public class Strategy {
+
+	
+}
